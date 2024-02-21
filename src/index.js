09c171c6e9679497350b466a21f342b7fca5c1b4
Swarnendu <swarnendumaity633@gmail.com>
2024-02-21 11:58:41 +0530

    Backend Setup

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({path: './.env'})
 
 
 connectDB()
+.then(()=>{
+    app.on("error", (err)=>{
+        console.log("Error: ",err);
+        throw err
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is runnning on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB Connection failed !!",err);
+})
 
 
 
