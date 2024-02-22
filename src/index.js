391b2818e667a2ee65df830a357a5d61d4b9f63b
Swarnendu <swarnendumaity633@gmail.com>
2024-02-22 18:05:07 +0530

    BackendControllers

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@
 
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 
 dotenv.config({path: './.env'})
 
 
 connectDB()
 .then(()=>{
-    app.on("error", (err)=>{
-        console.log("Error: ",err);
-        throw err
-    })
+    // app.on("error", (err)=>{
+    //     console.log("Error: ",err);
+    //     throw err
+    // })
     app.listen(process.env.PORT || 8000,()=>{
         console.log(`Server is runnning on port ${process.env.PORT}`);
     })
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
       statuscode,
       message="Something Went wrong ",
       errors = [],
-      statck=""
+      stack=""
     ){
         super(message)
         this.statuscode= statuscode
@@ -11,8 +11,8 @@ class ApiError extends Error{
         this.message = message
         this.success = false
         this.errors = errors
-        if (statck) {
-            this.stack=statck
+        if (stack) {
+            this.stack=stack
         }else{
             Error.captureStackTrace(this,this.constructor)
         }
